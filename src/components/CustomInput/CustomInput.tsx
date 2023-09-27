c92322841c4a1a6667a42adb6c9a68c6dc73cd8d
br14nn <vituallabrian727@gmail.com>
2023-09-27 14:20:07 +0800

    Adding grades form UI completed

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,13 +1,11 @@
 type TCustomInputProps = {
   className?: string;
   id?: string;
+  name?: string;
   type: React.HTMLInputTypeAttribute;
+  value?: string | number | readonly string[] | undefined;
 };
 
-export default function CustomInput({
-  className,
-  id,
-  type,
-}: TCustomInputProps) {
-  return <input className={`${className}`} id={id} type={type} />;
+export default function CustomInput({ className, id, name, type, value }: TCustomInputProps) {
+  return <input className={`${className}`} id={id} name={name} type={type} value={value} />;
 }
