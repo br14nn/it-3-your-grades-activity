cdf8b3da21bad28a3fd5c2a0aad1191595431fc5
Brian Velasco Vitualla <bvvitualla@addunet.ph>
2023-10-02 10:37:02 +0800

    form reset

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ export default function App() {
       return [gradesFormData, ...oldVal];
     });
 
+    e.currentTarget.reset();
     setGradesFormData({
       courseNumb: "",
       courseName: "",
